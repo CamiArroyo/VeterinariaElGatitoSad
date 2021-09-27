55076e70113ec33c3ff27352531227fd9010f715
Nahuel Facundo Fioramonti <nahuithewolfman@gmail.com>
2021-09-27 20:45:22 -0300

    Cambios pequeños
    Clases Vacuna y Laboratorio añadidas
    form a medio hacer de registrar sintoma
    atributos Nombre agregados a vacuna, sintoma y medicamento

---
+++ WindowsFormsApp1/WindowsFormsApp1/Interfaces/Sintomas/Registrar_Sintoma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Interfaces.Sintomas
+{
+    public partial class Frm_Registrar_Sintoma : Form
+    {
+        public Frm_Registrar_Sintoma()
+        {
+            InitializeComponent();
+        }
+
+        private void Registrar_Sintoma_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
